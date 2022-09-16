dc7039239251dff557f9d82b554c50c98eb08984
象数 <zhengxue.lzx@antgroup.com>
2022-09-16 16:29:57 +0800

    feat: 新增 AsyncSeriesBailHook

---
+++ src/core/AsyncSeriesBailhook.ts
@@ -0,0 +1,22 @@
+// @ts-ignore
+// tslint:disable-next-line:no-submodule-imports
+import series from 'async/series';
+import { CallBack } from './IHook';
+export default class AsyncSeriesHook  {
+  private tasks: any[];
+  private args: any[] = [];
+  constructor(...args: any[]) {
+    this.tasks = [];
+  }
+
+  public promise(...args: any[]): void {
+    this.args = args;
+    return series(this.tasks);
+  }
+  public tapPromise(name: string, cb: CallBack) {
+    this.tasks.push(async (callback: any) => {
+     const err= await cb(...this.args);
+      callback(err, name);
+    });
+  }
+}
